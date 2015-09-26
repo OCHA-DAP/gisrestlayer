73a0946e709b1c83190560fcdc4cc04d1d3b7f11
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2015-09-26 21:13:39 +0300

    Removing checks code and inserting dependency in requirements.txt

--- ckanext/hdx_service_checker/actions/authorize.py
+++
@@ -1,8 +0,0 @@
-from ckan.lib.base import _
-
-
-def run_checks(context, data_dict=None):
-    '''
-    Only sysadmins are allowed to call this action
-    '''
-    return {'success': False, 'msg': _('Only sysadmins can run checks')}
