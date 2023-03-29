5fbd899dbc93bdf046f04698dec2d85c1cf88787
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-03-29 14:19:56 +0300

    HDX-8773 changing default for redis stream host

--- eventapi/helpers/stream_redis.py
+++ eventapi/helpers/stream_redis.py
@@ -1,15 +1,17 @@
 import os
 import json
+import logging
 import redis
 
 # Connection to Redis
 redis_conn = None
+log = logging.getLogger(__name__)
 
 
 def stream_events_to_redis(event_list: [dict]):
     global redis_conn
     if redis_conn is None:
-        redis_stream_host = os.getenv('REDIS_STREAM_HOST', 'gisredis')
+        redis_stream_host = os.getenv('REDIS_STREAM_HOST', 'redis')
         redis_stream_port = os.getenv('REDIS_STREAM_PORT', 6379)
         redis_stream_db = os.getenv('REDIS_STREAM_DB', 7)
 
@@ -20,4 +22,6 @@ def stream_events_to_redis(event_list: [dict]):
             'event_type': event['event_type'],
             'body': bytes(json.dumps(event), 'utf-8')
         }
+        log.info('Processing event type {}'.format(event['event_type']))
         redis_conn.xadd('hdx_event_stream', processed_event)
+        log.info('Finished processing event type {}'.format(event['event_type']))
