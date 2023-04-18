ca50d8e775b866300a790a35b451759ac898ca5d
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2023-04-18 23:14:43 +0300

    HDX-8773 sending events to n8n webhook

--- eventapi/read_redis_stream.py
+++ eventapi/read_redis_stream.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+
+import requests
 from hdx_redis_lib import connect_to_hdx_event_bus, RedisKeyValueStore, RedisConfig
 
 parser = argparse.ArgumentParser()
@@ -7,6 +9,7 @@ parser.add_argument('--host', type=str, default='redis', help='Redis host (defau
 parser.add_argument('--port', type=int, default=6379, help='Redis port (default: 6379)')
 parser.add_argument('--db', type=int, default=7, help='Redis database number (default: 7)')
 parser.add_argument('--clean', type=bool, default=True, help='Clean stream after reading (default: True)')
+parser.add_argument('--n8n-webhook', type=str, default=None, help='POST webhook to receive the event (default: None)')
 args = parser.parse_args()
 
 # Define the stream name and the ID to start reading from
@@ -21,15 +24,31 @@ event_bus = connect_to_hdx_event_bus(
     stream_name, group_name, consumer_name,
     RedisConfig(host=args.host, port=args.port, db=args.db)
 )
+
+
+def event_processor(event):
+    print(json.dumps(event, ensure_ascii=False, indent=4))
+    if args.n8n_webhook:
+        event_list = {'events': [event]}
+        try:
+            result = requests.post(args.n8n_webhook, data=json.dumps(event_list), headers={'Content-type': 'application/json'})
+            result.raise_for_status()
+        except Exception as e:
+            print(str(e))
+    return True, 'Success'
+
+
+event_bus.hdx_listen(event_processor)
+
 # kv = RedisKeyValueStore(RedisConfig(host=args.host, port=args.port, db=args.db))
 
 # Continuously read events from the stream
-while True:
+# while True:
     # Read events from the stream starting from the last ID
     # events = r.xread({stream_name: last_id}, block=5000)
-    event = event_bus.read_hdx_event()
-    if event:
-        print(json.dumps(event, ensure_ascii=False, indent=4))
+    # event = event_bus.read_hdx_event()
+    # if event:
+    #     print(json.dumps(event, ensure_ascii=False, indent=4))
 
     # Process each event
     # for event_tuple in events:
