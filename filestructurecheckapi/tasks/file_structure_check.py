7c2b2316eae3d8b200a93aeecdc01ba5f1c41820
Dan Mihaila <danmihaila@gmail.com>
2026-01-28 11:50:02 +0200

    HDX-11324 fs_check better reporting

--- filestructurecheckapi/tasks/file_structure_check.py
+++ filestructurecheckapi/tasks/file_structure_check.py
@@ -1,13 +1,14 @@
 import logging
 import json
+from typing import List, Dict, Any, Optional
 import requests
 import os
 import datetime
+from requests import Response
 import filestructurecheckapi.exceptions.exceptions as exceptions
 import filestructurecheckapi.helpers.detect_changes as dc
 import importapi.exceptions.exceptions as import_api_exceptions
 
-from urllib.error import HTTPError
 from json import JSONDecodeError
 
 logger = logging.getLogger(__name__)
@@ -17,18 +18,20 @@ def fs_check_task(task_arguments):
     task = FSCheckTask(task_arguments)
     return task.process()
 
+STATE_SUCCESS = "success"
+STATE_ERROR = "error"
+KEY_FS_CHECK_INFO = "fs_check_info"
 
 class FSCheckTask(object):
-    def __init__(self, args):
+
+    def __init__(self, args: dict) -> None:
         self.dataset_id = args.get('dataset_id')
         self.resource_id = args.get('resource_id')
-        # self.download_url = args.get('download_url')
         self.verify_ckan_ssl = args.get('verify_ckan_ssl')
         self.ckan_server_url = args.get('ckan_server_url')
-        # self.url_type = args.get('url_type')
         self.timeout = args.get('timeout_sec')
         self.hxl_proxy_source_info_url = args.get('hxl_proxy_source_info_url')
-        self.fs_check_info = args.get('fs_check_info')
+        self.fs_check_info = args.get(KEY_FS_CHECK_INFO)
         # TODO remove testing api_key
         self.api_key = os.getenv('HDX_FSCHECK_API_KEY', os.getenv('HDX_GIS_API_KEY'))
         self.resource_update_api = '{}/{}'.format(args['ckan_api_base_url'], args['resource_update_action'])
@@ -38,42 +41,79 @@ class FSCheckTask(object):
             'Authorization': self.api_key
         }
 
-    def process(self):
+    def _resolve_state_and_message(self, fs_check_info_dict: dict) -> tuple[str, str]:
+        if fs_check_info_dict.get("error"):
+            return STATE_ERROR, fs_check_info_dict.get(
+                "message", "File structure check failed"
+            )
+        return STATE_SUCCESS, "File structure check completed"
+
+    def process(self) -> Optional[Response]:
         logger.info(
             "In fs_check for {}, {}".format(self.dataset_id, self.resource_id))
+        fs_check_info_dict = {}
         try:
             # hxl_proxy_source_info_url = self.hxl_proxy_source_info_url
             # TODO uncomment next line and comment text line
-            response = requests.get(self.hxl_proxy_source_info_url, allow_redirects=True)
+            response = requests.get(self.hxl_proxy_source_info_url, allow_redirects=True, timeout=self.timeout)
+            response.raise_for_status()
             # DEBUG - for local env
             # response = requests.get(
             #     'https://data.humdata.org/hxlproxy/api/source-info?url=https://data.humdata.org/dataset/6c4c69cf-8ca0-4bfc-8c46-73cdb18812d5/resource/cfe1321e-89ce-43f3-b067-6da4bbb3ca80/download/somalia-2022-post-gu-total-acute-malnutrition-burden-and-prevalence-for-aug-2022-to-jul-2023-by.xlsx',
             #     allow_redirects=True)
             logger.info("task done")
-            data_dict = json.loads(response.text)
-            sheet_changes = self.process_fs_check_info_changes(data_dict)
-            self.push_information_back_to_ckan(data_dict, sheet_changes)
+            fs_check_info_dict = json.loads(response.text)
+            sheet_changes = self.process_fs_check_info_changes(fs_check_info_dict)
+            state, message = self._resolve_state_and_message(fs_check_info_dict)
+            self.push_information_back_to_ckan(
+                fs_check_info_dict,
+                sheet_changes,
+                state=state,
+                message=message,
+            )
             return response
         except JSONDecodeError as ex:
             logger.warning(ex)
-        except HTTPError as ex:
-            logger.warning(ex)
+            self.push_information_back_to_ckan(
+                fs_check_info_dict={"error": "JSONDecodeError", "details": str(ex)},
+                sheet_changes=[],
+                state=STATE_ERROR,
+                message=str(ex),
+            )
+            return None
+        except (requests.exceptions.HTTPError, requests.exceptions.RequestException) as ex:
+            logger.warning(f"HTTP request failed: {ex}")
+            self.push_information_back_to_ckan(
+                fs_check_info_dict={"error": ex.__class__.__name__, "details": str(ex)},
+                sheet_changes=[],
+                state=STATE_ERROR,
+                message=str(ex),
+            )
+            return None
         except Exception as ex:
             logger.error(ex)
+            self.push_information_back_to_ckan(
+                fs_check_info_dict={"error": ex.__class__.__name__, "details": str(ex)},
+                sheet_changes=[],
+                state=STATE_ERROR,
+                message=str(ex),
+            )
             raise exceptions.HXLProxyException('hxl proxy error/exception')
 
-    def push_information_back_to_ckan(self, fs_check_info_dict, sheet_changes):
+    def push_information_back_to_ckan(self, fs_check_info_dict: dict, sheet_changes: list, state: str = STATE_SUCCESS, message: str = "File structure check completed"):
 
         if self.resource_update_api and self.api_key:
             try:
-                fs_check_info_json = json.dumps(fs_check_info_dict)
+                # fs_check_info_json = json.dumps(fs_check_info_dict)
+                if 'details' in fs_check_info_dict:
+                    fs_check_info_dict['details'] = fs_check_info_dict['details'].replace("http://hxl:5000", "")
                 data_json = json.dumps({
                     'id': self.resource_id,
                     'package_id': self.dataset_id,
-                    'key': 'fs_check_info',
+                    'key': KEY_FS_CHECK_INFO,
                     'value': {
-                        "state": "success",
-                        "message": "File structure check completed",
+                        "state": state,
+                        "message": message.replace("http://hxl:5000", ""),
                         "timestamp": datetime.datetime.now().isoformat(),
                         "sheet_changes": sheet_changes,
                         "hxl_proxy_response": fs_check_info_dict
@@ -97,20 +137,20 @@ class FSCheckTask(object):
                     self.resource_id))
             raise import_api_exceptions.WrongConfigurationException('Either CKAN resource update url or import api key missing')
 
-    def process_fs_check_info_changes(self, data_dict):
+    def process_fs_check_info_changes(self, data_dict: Dict) -> List[Dict[str, Any]]:
         sheet_changes = []
         if self.fs_check_info:
             _fs_check_info = json.loads(self.fs_check_info)
-            success_item_list = [item for item in _fs_check_info if item.get('state') == 'success']
-            if success_item_list and len(success_item_list)>=1:
+            success_item_list = [item for item in _fs_check_info if item.get('state') == STATE_SUCCESS]
+            if success_item_list:
                 last_success_item = success_item_list[-1]
                 current_item = {
-                    'state': 'success',
+                    'state': STATE_SUCCESS,
                     'message': '',
                     'timestamp':'',
                     'hxl_proxy_response': data_dict
                 }
-                file_structure_event_list = dc.detect_file_structure_changes( current_item, last_success_item)
+                file_structure_event_list = dc.detect_file_structure_changes(current_item, last_success_item)
                 for fse in file_structure_event_list:
                     sheet_changes.append({
                         'name':fse.sheet_id,
